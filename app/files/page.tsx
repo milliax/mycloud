8eacfe8fa88c423979342e571e8755c8afb45488
milliax <wei@sivir.pw>
2023-11-26 18:24:35 +0800

    udpate

---
+++ app/files/page.tsx
@@ -0,0 +1,7 @@
+export default function Files() {
+    return (
+        <main>
+            Files
+        </main>
+    )
+}
---
+++ app/photos/page.tsx
@@ -0,0 +1,7 @@
+export default function Photos() {
+    return (
+        <main>
+            Photos
+        </main>
+    )
+}
