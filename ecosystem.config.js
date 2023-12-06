38ab77cec7f5284c517dcc3770900fdf13e87277
milliax <wei@sivir.pw>
2023-12-07 04:17:18 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,22 @@
 module.exports = {
     apps: [{
         name: "milliax cloud",
-        script: "yarn",
+        script: "pnpm",
         args: "start",
-        instances: 'max',
+        instances: 1,
         exec_mode: 'cluster',
         autorestart: true,
         max_memory_restart: '1G',
         watch: true,
         out_file: '../logs/out.log',
         error_file: '../logs/error.log',
-        // env_production: {
-        //     NODE_ENV: "production",
-        //     PORT: 5000
-        // }
-        env: {
+        env_production: {
             NODE_ENV: "production",
-            PORT: "5002",
+            PORT: 5002
+        },
+        env_preview: {
+            NODE_ENV: "production",
+            PORT: 5003,
         }
     }]
 }
